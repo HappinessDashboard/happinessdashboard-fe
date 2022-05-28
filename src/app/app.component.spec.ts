f23969625b23d0599ab49501ca2fbeeb4bad3e16
Valcan-Andrada <andrada.valcan01@e-uvt.ro>
2022-05-28 20:40:21 +0300

    com

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'PC-FE'`, () => {
+  it(`should have as title 'HappinessDashboard'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('PC-FE');
+    expect(app.title).toEqual('HappinessDashboard');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('PC-FE app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('HappinessDashboard app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MenuComponent } from './components/menu/menu.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AnswerComponent } from './components/answer/answer.component';
+import { CreateComponent } from './components/create/create.component';
+import { QuestionComponent } from './components/question/question.component';
+import { FormsModule } from '@angular/forms';
+//import { PollService } from './poll.service';
+
+
+const routes: Routes = [
+  {path: '', component:HomepageComponent},
+  {path: 'Answer', component: AnswerComponent},
+  {path: 'Create', component: CreateComponent},
+  {path: 'Question', component: QuestionComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomepageComponent,
+    AnswerComponent,
+    CreateComponent,
+    QuestionComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    FormsModule
   ],
-  providers: [],
+  providers: [],//[PollService],
+  exports: [RouterModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
